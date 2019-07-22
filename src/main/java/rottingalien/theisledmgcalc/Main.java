b304d5ef2024b6a133001c7234c2dc7081a15e01
RottingAlien <cradleofcorpses@gmail.com>
2019-07-22 01:58:59 +0100

    Separated spring values more

--- src/main/java/rottingalien/theisledmgcalc/Main.java
+++ src/main/java/rottingalien/theisledmgcalc/Main.java
@@ -4,25 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import rottingalien.theisledmgcalc.dinoinfo.Dino;
 import rottingalien.theisledmgcalc.dinoinfo.GrowthState;
-import rottingalien.utils.Maths.FindMidValues;
-
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context =
-                new FileSystemXmlApplicationContext(
-                        "/src/main/resources/spring/config.xml"
-                );
-
+        ApplicationContext context = new FileSystemXmlApplicationContext("/src/main/resources/spring/config.xml");
 
 
         Dino carnotaurus = (Dino) context.getBean("carnotaurus");
 
-        for (GrowthState growthState : carnotaurus.getGrowthStateMap().values()){
+        for (GrowthState growthState : carnotaurus.getGrowthStateMap().values()) {
             System.out.println(carnotaurus.getName() + " " + growthState.getName());
-            for (Integer value: growthState.getDinoPropertyMap().values()) {
+            for (Integer value : growthState.getDinoPropertyMap().values()) {
                 System.out.println(value);
             }
 
